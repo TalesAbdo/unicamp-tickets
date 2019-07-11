b1101c3f4f5c4613e6bf7ca0106aab80a752a92b
Tales Abdo dos Santos <tales.abdo@ibm.com>
2019-07-11 09:04:55 -0300

    Lazy load routes

--- src/router.js
+++ src/router.js
@@ -1,11 +1,11 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Login from 'view/Login/Login.vue';
-import Home from 'view/Home/Home.vue';
-import Tickets from 'view/Tickets/Tickets.vue';
-import Services from 'view/Services/Services.vue';
-import Members from 'view/Members/Members.vue';
-import Metrics from 'view/Metrics/Metrics.vue';
+// import Login from 'view/Login/Login.vue';
+// import Home from 'view/Home/Home.vue';
+// import Tickets from 'view/Tickets/Tickets.vue';
+// import Services from 'view/Services/Services.vue';
+// import Members from 'view/Members/Members.vue';
+// import Metrics from 'view/Metrics/Metrics.vue';
 
 Vue.use(Router);
 
@@ -14,37 +14,32 @@ export default new Router({
         {
             path: '/',
             name: 'login',
-            component: Login,
+            component: () => import(/* webpackChunkName: "about" */ 'view/Login/Login.vue'),
         },
         {
             path: '/home',
             name: 'Home',
-            component: Home,
+            component: () => import(/* webpackChunkName: "about" */ 'view/Home/Home.vue'),
         },
         {
             path: '/tickets',
             name: 'Tickets',
-            component: Tickets,
+            component: () => import(/* webpackChunkName: "about" */ 'view/Tickets/Tickets.vue'),
         },
         {
             path: '/services',
             name: 'Services',
-            component: Services,
+            component: () => import(/* webpackChunkName: "about" */ 'view/Services/Services.vue'),
         },
         {
             path: '/members',
             name: 'Members',
-            component: Members,
+            component: () => import(/* webpackChunkName: "about" */ 'view/Members/Members.vue'),
         },
         {
             path: '/metrics',
             name: 'Metrics',
-            component: Metrics,
+            component: () => import(/* webpackChunkName: "about" */ 'view/Metrics/Metrics.vue'),
         },
-        // {
-        //     path: '/about',
-        //     name: 'about',
-        //     component: () => import(/* webpackChunkName: "about" */ './viewComponents/About.vue'),
-        // },
     ],
 });
